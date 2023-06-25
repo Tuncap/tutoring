bd63261b35f3f0272324969066cd588a1c213102
Chris <cktuncap@gmail.com>
2023-06-25 00:06:05 -0500

    using print writer and move exceptions

--- src/fileReadingWriting/FileProgram.java
+++ src/fileReadingWriting/FileProgram.java
@@ -3,25 +3,22 @@ package assingments;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class FileProgram {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, FileNotFoundException {
 		System.out.println("Welcomet to the value reader application. \n"
 				+ "This program will load value.txt from disk.  If it does not exist then one will be created.\n ");
 
 		// Read the content from file
 		String fileContents = "";
 
-		try {
-			FileReader fileReader = new FileReader("value.txt");
-			Scanner myReader = new Scanner(fileReader);
-			while (myReader.hasNextLine()) {
-				fileContents = fileContents + myReader.nextLine();
-			}
-
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found");
+		FileReader fileReader = new FileReader("value.txt");
+		Scanner myReader = new Scanner(fileReader);
+		while (myReader.hasNextLine()) {
+			fileContents = fileContents + myReader.nextLine();
 		}
 
 		System.out.println("Value stored in file (if found): " + fileContents);
@@ -30,20 +27,17 @@ public class FileProgram {
 		String userEnteredValue = scanner.nextLine();
 
 		System.out.println("Cool.  Writing " + userEnteredValue + " to a value.txt");
-		try {
-			// Creates a FileWriter
-			FileWriter output = new FileWriter("value.txt");
 
-			// Writes the string to the file
-			output.write(userEnteredValue);
+		// Creates a FileWriter
+		FileWriter output = new FileWriter("value.txt");
+		PrintWriter pw = new PrintWriter(output);
+		// Writes the string to the file
+		pw.printf(userEnteredValue);
 
-			// Closes the writer
-			output.close();
-			System.out.println("file saved");
-		}
-		catch (Exception e) {
-			System.out.println("error saving file");
-		}
+		// Closes the writer
+		output.close();
+		System.out.println("file saved");
+
 		scanner.close();
 	}
 
